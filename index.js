948c4d5af16ec3cd0eac450dbfe07a490158ac26
ThurDev <thurcoder@gmail.com>
2023-04-07 23:46:53 +0100

    Now verify if the modal is in english as well :)

--- index.js
+++ index.js
@@ -32,7 +32,10 @@
 
       let modalTitle = document.querySelector("h4.modal-title").innerText;
 
-      if (modalTitle == "Compra concluída") {
+      if (
+        modalTitle == "Compra concluída" ||
+        modalTitle == "Purchase Complete"
+      ) {
         keepTrying = false;
         window.location.reload();
       }
