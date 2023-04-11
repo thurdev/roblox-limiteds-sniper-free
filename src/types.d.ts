b02a9cad6f65358af392d430b467a14a7094ef37
Facal <facaldev@gmail.com>
2023-04-11 15:48:39 -0300

    🔧 chore(.eslintrc.js): add eslint configuration file 🔧 chore(.prettierrc): add prettier configuration file 🔧 chore(package.json): add format and lint scripts, update devDependencies 🔧 chore(src/__puppeteer.d.ts): remove semicolons 🔧 chore(src/global.d.ts): remove semicolons
    ✨ feat(index.ts): add support for buying items with non-zero price
    🐛 fix(index.ts): fix console.log statements to use template literals instead of concatenation
    🚨 chore(index.ts): fix linting issues and formatting
    
    🎨 style(types.d.ts): format code and remove semicolons
    
    🐛 fix(actions.ts): remove unused import and fix syntax errors in buy function
    
    🔨 refactor(preparedRequests.ts): change quotes to single quotes and remove semicolons
    🐛 fix(preparedRequests.ts): fix typo in getItemDetails parameter name
    ✨ feat(preparedRequests.ts): add support for getting marketplace item details
    
    🎨 style(roblox.ts, timers.ts): fix code formatting and remove unnecessary semicolons
    
    🔧 chore(token.ts): refactor token.ts to use ES6 imports instead of require
    🔧 chore(tsconfig.json): exclude node_modules from TypeScript compilation

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,60 +1,60 @@
-type ItemType = "Asset" | "GamePass";
+type ItemType = 'Asset' | 'GamePass'
 
 export type Item = {
-  id: number;
-  itemType: ItemType;
-};
+  id: number
+  itemType: ItemType
+}
 
 export type ItemDetails = {
-  assetType: number;
-  collectibleItemId: string;
-  creatorHasVerifiedBadge: boolean;
-  creatorName: string;
-  creatorTargetId: number;
-  creatorType: string;
-  description: string;
-  favoriteCount: number;
-  genres: string[];
-  id: number;
-  itemRestrictions: string[];
-  itemStatus: string[];
-  itemType: ItemType;
-  lowestPrice: number;
-  name: string;
-  offSaleDeadline?: unknown;
-  price: number;
-  totalQuantity: number;
-  priceStatus: string;
-  unitsAvailableForConsumption: number;
-  productId: number;
-};
+  assetType: number
+  collectibleItemId: string
+  creatorHasVerifiedBadge: boolean
+  creatorName: string
+  creatorTargetId: number
+  creatorType: string
+  description: string
+  favoriteCount: number
+  genres: string[]
+  id: number
+  itemRestrictions: string[]
+  itemStatus: string[]
+  itemType: ItemType
+  lowestPrice: number
+  name: string
+  offSaleDeadline?: unknown
+  price: number
+  totalQuantity: number
+  priceStatus: string
+  unitsAvailableForConsumption: number
+  productId: number
+}
 
 export type MarketPlaceItemDetail = {
-  assetStock: number;
-  collectibleItemId: string;
-  collectibleProductId: string;
-  creatorHasVerifiedBadge: boolean;
-  creatorId: number;
-  creatorName: string;
-  creatorType: string;
-  description: string;
-  errorCode: null;
-  itemRestrictions: null;
-  itemTargetId: number;
-  lowestPrice: number;
-  name: string;
-  offSaleDeadline: string;
-  price: number;
-  unitsAvailableForConsumption: number;
-};
+  assetStock: number
+  collectibleItemId: string
+  collectibleProductId: string
+  creatorHasVerifiedBadge: boolean
+  creatorId: number
+  creatorName: string
+  creatorType: string
+  description: string
+  errorCode: null
+  itemRestrictions: null
+  itemTargetId: number
+  lowestPrice: number
+  name: string
+  offSaleDeadline: string
+  price: number
+  unitsAvailableForConsumption: number
+}
 
 export type CurrentUser = {
-  displayName: string;
-  hasVerifiedBadge: boolean;
-  is13orOver: boolean;
-  isAuthenticated: boolean;
-  isPremiumUser: boolean;
-  isUnder13: false;
-  name: string;
-  userId: string;
-};
+  displayName: string
+  hasVerifiedBadge: boolean
+  is13orOver: boolean
+  isAuthenticated: boolean
+  isPremiumUser: boolean
+  isUnder13: false
+  name: string
+  userId: string
+}
--- src/utils/preparedRequests.ts
+++ src/utils/preparedRequests.ts
@@ -1,112 +1,109 @@
-import axios from "axios";
-import { Item, ItemDetails, MarketPlaceItemDetail } from "../types";
-import { generateXCSRFToken } from "./token";
+import axios from 'axios'
+import { Item, ItemDetails, MarketPlaceItemDetail } from '../types'
+import { generateXCSRFToken } from './token'
 
 export const getItems = async (): Promise<Item[]> => {
   const config = {
-    method: "get",
-    url: "https://catalog.roblox.com/v1/search/items?category=All&limit=120&maxPrice=0&minPrice=0&salesTypeFilter=2&sortType=4", // free limiteds
+    method: 'get',
+    url: 'https://catalog.roblox.com/v1/search/items?category=All&limit=120&maxPrice=0&minPrice=0&salesTypeFilter=2&sortType=4', // free limiteds
     headers: {
-      "user-agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       cookie: process.env.ROBLOX_COOKIES,
     },
-  };
+  }
   const response = await axios(config).catch((err) => {
-    console.log("Could not get items", JSON.stringify(err.response.data));
-    return err.response;
-  });
-  return response.data.data;
-};
+    console.log('Could not get items', JSON.stringify(err.response.data))
+    return err.response
+  })
+  return response.data.data
+}
 
 export const getItemDetails = async (
   itemData:
     | Item
     | {
-        itemType: string;
-        id: string;
+        itemType: string
+        id: string
       }
 ): Promise<ItemDetails> => {
   const config = {
-    method: "post",
-    url: "https://catalog.roblox.com/v1/catalog/items/details",
+    method: 'post',
+    url: 'https://catalog.roblox.com/v1/catalog/items/details',
     headers: {
-      "user-agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       cookie: process.env.ROBLOX_COOKIES,
-      "x-csrf-token": await generateXCSRFToken(),
+      'x-csrf-token': await generateXCSRFToken(),
     },
     data: {
       items: [itemData],
     },
-  };
+  }
 
   const response = await axios(config).catch((err) => {
-    console.log(
-      "Could not get item details",
-      JSON.stringify(err.response.data)
-    );
-    return err.response;
-  });
-  return response.data.data[0];
-};
+    console.log('Could not get item details', JSON.stringify(err.response.data))
+    return err.response
+  })
+  return response.data.data[0]
+}
 
 export const getMarketplaceDetails = async (
   ids: string
 ): Promise<MarketPlaceItemDetail> => {
   const config = {
-    method: "post",
-    url: "https://apis.roblox.com/marketplace-items/v1/items/details",
+    method: 'post',
+    url: 'https://apis.roblox.com/marketplace-items/v1/items/details',
     headers: {
-      "user-agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       cookie: process.env.ROBLOX_COOKIES,
-      "x-csrf-token": await generateXCSRFToken(),
+      'x-csrf-token': await generateXCSRFToken(),
     },
     data: {
       itemIds: [ids],
     },
-  };
+  }
 
   const response = await axios(config).catch((err) => {
     console.log(
-      "Could not get marketplace item details, probably because its not for sale yet.",
+      'Could not get marketplace item details, probably because its not for sale yet.',
       JSON.stringify(err.response.data)
-    );
-    return err.response;
-  });
-  return response.data[0];
-};
+    )
+    return err.response
+  })
+  return response.data[0]
+}
 
 export const buyItem = async (payload: { [key: string]: unknown }) => {
   const config = {
-    method: "post",
+    method: 'post',
     url: `https://apis.roblox.com/marketplace-sales/v1/item/${payload.collectibleItemId}/purchase-item`,
     headers: {
-      authority: "economy.roblox.com",
-      accept: "application/json, text/plain, */*",
-      "accept-language": "pt-BR,pt;q=0.9",
-      "content-type": "application/json;charset=UTF-8",
+      authority: 'economy.roblox.com',
+      accept: 'application/json, text/plain, */*',
+      'accept-language': 'pt-BR,pt;q=0.9',
+      'content-type': 'application/json;charset=UTF-8',
       cookie: process.env.ROBLOX_COOKIES,
-      origin: "https://www.roblox.com",
-      referer: "https://www.roblox.com/",
-      "sec-ch-ua":
+      origin: 'https://www.roblox.com',
+      referer: 'https://www.roblox.com/',
+      'sec-ch-ua':
         '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-      "x-csrf-token": await generateXCSRFToken(),
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+      'x-csrf-token': await generateXCSRFToken(),
     },
     data: payload,
-  };
+  }
 
   const response = await axios(config).catch((err) => {
-    console.log("Could not buy item", JSON.stringify(err.response.data));
-    return err;
-  });
-  return response.data;
-};
+    console.log('Could not buy item', JSON.stringify(err.response.data))
+    return err
+  })
+  return response.data
+}
